1bc7c58cdb469373da44516dda2b1447b75777ef
NurievMikhail <nurievmm@yandex.ru>
2017-12-09 15:12:44 +0500

    Решение задачи.

--- client-core.js
+++ client-core.js
@@ -1,11 +1,176 @@
 'use strict';
 
+const request = require('request');
+const querystring = require('querystring');
+const chalk = require('chalk');
+
 module.exports.execute = execute;
 module.exports.isStar = true;
 
-function execute() {
-    // Внутри этой функции нужно получить и обработать аргументы командной строки
-    const args = process.argv;
+const URL = 'http://localhost:8080/messages';
 
-    return Promise.resolve('Это строка будет выведена на консоль');
+async function execute() {
+    const parsedArgs = parseArgs(process.argv);
+    const query = createFromToQuery(parsedArgs);
+    let resultString = '';
+    switch (parsedArgs.command.toLowerCase()) {
+        case 'send':
+            resultString = dyeResponse(await sendHelper(parsedArgs, query), parsedArgs.detailed);
+
+            return Promise.resolve(resultString);
+        case 'list':
+            resultString = (await listHelper(query))
+                .map((message) => (dyeResponse(message, parsedArgs.detailed)))
+                .join('\n\n');
+
+            return Promise.resolve(resultString);
+        case 'delete':
+            resultString = await deleteHelper(parsedArgs);
+
+            return Promise.resolve(resultString);
+        case 'edit':
+            resultString = dyeResponse(await editHelper(parsedArgs), parsedArgs.detailed);
+
+            return Promise.resolve(resultString);
+        default:
+            return;
+    }
+}
+
+function dyeResponse(response, detailed) {
+    let resultResponse = '';
+    if (detailed) {
+        let id = chalk.hex('#FF0')('ID');
+        resultResponse += `${id}: ${response.id}\n`;
+    }
+    if (response.from) {
+        let from = chalk.hex('#F00')('FROM');
+        resultResponse += `${from}: ${response.from}\n`;
+    }
+    if (response.to) {
+        let to = chalk.hex('#F00')('TO');
+        resultResponse += `${to}: ${response.to}\n`;
+    }
+    let text = chalk.hex('#0F0')('TEXT');
+    resultResponse += `${text}: ${response.text}`;
+    if (response.edited) {
+        resultResponse += `${chalk.hex('#777')('(edited)')}`;
+    }
+
+    return resultResponse;
+}
+
+function sendHelper(args, query) {
+    if (!args.text) {
+        return;
+    }
+    let resultUrl = URL;
+    if (query) {
+        resultUrl += `?${query}`;
+    }
+    const options = {
+        method: 'POST',
+        url: resultUrl,
+        body: { text: args.text },
+        json: true
+    };
+
+    return createRequestPromise(options);
+}
+
+function listHelper(query) {
+    let resultUrl = URL;
+    if (query) {
+        resultUrl += `?${query}`;
+    }
+    const options = {
+        method: 'GET',
+        url: resultUrl,
+        json: true
+    };
+
+    return createRequestPromise(options);
+}
+
+function deleteHelper(args) {
+    if (!args.id) {
+        return;
+    }
+    let resultUrl = `${URL}/${args.id}`;
+    const options = {
+        method: 'DELETE',
+        url: resultUrl,
+        json: true
+    };
+
+    return new Promise((resolve) => {
+        request(options, function () {
+            return resolve('DELETED');
+        });
+    });
+}
+
+function editHelper(args) {
+    if (!args.text || !args.id) {
+        return;
+    }
+    let resultUrl = `${URL}/${args.id}`;
+    const options = {
+        method: 'PATCH',
+        url: resultUrl,
+        body: { text: args.text },
+        json: true
+    };
+
+    return createRequestPromise(options);
+}
+
+function createFromToQuery(obj) {
+    let fromToQuery = {};
+    if (obj.from) {
+        fromToQuery.from = obj.from;
+    }
+    if (obj.to) {
+        fromToQuery.to = obj.to;
+    }
+
+    return querystring.stringify(fromToQuery);
+}
+
+function parseArgs(args) {
+    let resultParams = {
+        command: args[2]
+    };
+    let params = args.slice(3);
+    for (let i = 0; i < params.length; i++) {
+        let paramName = '';
+        let paramValue = '';
+        if (/-v/.test(params[i])) {
+            resultParams.detailed = true;
+            continue;
+        } else if (/--.+=.+/.test(params[i])) {
+            paramName = params[i].split('=')[0].slice(2);
+            paramValue = params[i].split('=')[1];
+        } else {
+            paramName = params[i].slice(2);
+            paramValue = params[i + 1];
+            i++;
+        }
+        resultParams[paramName] = paramValue;
+    }
+
+    return resultParams;
+}
+
+
+function createRequestPromise(options) {
+    return new Promise((resolve) => {
+        request(options, function (err, res, body) {
+            if (err) {
+                return resolve(err);
+            }
+
+            return resolve(body);
+        });
+    });
 }
--- server-core.js
+++ server-core.js
@@ -1,12 +1,103 @@
 'use strict';
 
 const http = require('http');
+const url = require('url');
+const shortid = require('shortid');
 
-const server = http.createServer();
+const server = new http.Server();
+let chat = [];
 
-server.on('request', (req, res) => {
-    // Тут нужно обработать запрос
-    res.end();
+server.on('request', function (req, res) {
+    let parsedUrl = url.parse(req.url, true);
+    if (/\/messages/.test(parsedUrl.pathname)) {
+        res.setHeader('Content-Type', 'application/json');
+        let messageText = '';
+        let id = '';
+        switch (req.method) {
+            case 'GET':
+                res.end(getHelper(parsedUrl.query.from, parsedUrl.query.to));
+                break;
+            case 'POST':
+                req.on('readable', function () {
+                    let partOfMessage = req.read();
+                    if (partOfMessage !== null) {
+                        messageText += partOfMessage;
+                    }
+                });
+                req.on('end', function () {
+                    messageText = JSON.parse(messageText);
+                    res.end(postHelper(
+                        parsedUrl.query.from, parsedUrl.query.to, messageText.text));
+                });
+                break;
+            case 'DELETE':
+                id = parsedUrl.href.split('/')[2];
+                res.end(deleteHelper(id));
+                break;
+            case 'PATCH':
+                id = parsedUrl.href.split('/')[2];
+                req.on('readable', function () {
+                    let partOfMessage = req.read();
+                    if (partOfMessage !== null) {
+                        messageText += partOfMessage;
+                    }
+                });
+                req.on('end', function () {
+                    messageText = JSON.parse(messageText);
+                    res.end(patchHelper(id, messageText.text));
+                });
+                break;
+            default:
+                res.statusCode = 404;
+                res.end();
+                break;
+        }
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
 });
+
+function getHelper(from, to) {
+    if (from === undefined && to === undefined) {
+        return JSON.stringify(chat);
+    }
+    if (to === undefined) {
+        return JSON.stringify(chat.filter((chatMessage) => (chatMessage.from === from)));
+    }
+    if (from === undefined) {
+        return JSON.stringify(chat.filter((chatMessage) => (chatMessage.to === to)));
+    }
+
+    return JSON.stringify(chat.filter((chatMessage) =>
+        (chatMessage.to === to && chatMessage.from === from)));
+}
+
+function postHelper(from, to, text) {
+    let newMessage = {
+        id: shortid.generate(),
+        from: from || undefined,
+        to: to || undefined,
+        text: text
+    };
+    chat.push(newMessage);
+
+    return JSON.stringify(newMessage);
+}
+
+function deleteHelper(id) {
+    let indexOfMessage = chat.indexOf(chat.find((message) => (message.id === id)));
+    chat.splice(indexOfMessage, 1);
+
+    return JSON.stringify({ 'status': 'ok' });
+}
+
+function patchHelper(id, text) {
+    let patchedMessage = chat.find((message) => (message.id === id));
+    patchedMessage.text = text;
+    patchedMessage.edited = true;
+
+    return JSON.stringify(patchedMessage);
+}
 
 module.exports = server;
